ffb93d2819450ca0cd260416f55d5381ec9751f9
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-05-20 08:50:41 +0700

    Update install_to_bin.sh

--- install_to_bin.sh
+++ install_to_bin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "[i] We will now install Raven-Storm to your bin path..."
+echo "[i] Bây giờ chúng tôi sẽ cài đặt Raven-Storm vào đường dẫn bin của bạn..."
 if [ -d "/usr/share/Raven-Storm" ] ; then
-    echo "[i] Found an old version of Raven-Storm, proceeding to update..."
-    echo "[i] Backing up old verison."
+    echo "[i] Đã tìm thấy phiên bản cũ của Raven-Storm, đang tiếp tục cập nhật..."
+    echo "[i] Sao lưu phiên bản cũ."
     if [ -d "/usr/share/Raven-Storm/Backup" ] ; then
         sudo mv /usr/share/Raven-Storm/Backup ./Backup
     else
@@ -19,22 +19,23 @@ if [ -d "/usr/share/Raven-Storm" ] ; then
     sudo mv /usr/share/Raven-Storm $name
     mv ./Backup ./Raven-Storm/
     sudo cp -ar ./Raven-Storm /usr/share/
-    echo "[i] Installation sucessful."
-    echo "[i] Making Raven-Storm executable..."
+    echo "[i] cài đặt thành công."
+    echo "[i] Làm cho Raven-Storm có thể thực thi..."
     sudo mv /usr/share/Raven-Storm/main.py /usr/share/Raven-Storm/rst
     sudo chmod +x /usr/share/Raven-Storm/rst
-    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Liên kết dường như đã tồn tại."
 else
     sudo cp -ar ./Raven-Storm /usr/share/
-    echo "[i] Installation sucessful."
-    echo "[i] Making Raven-Storm executable..."
+    echo "[i] cài đặt thành công."
+    echo "[i] Làm cho Raven-Storm có thể thực thi..."
     sudo mv /usr/share/Raven-Storm/main.py /usr/share/Raven-Storm/rst
     sudo chmod +x /usr/share/Raven-Storm/rst
-    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Liên kết dường như đã tồn tại."
 fi
 
-echo "[i] You can delete the Raven-Storm folder now."
+echo "[i] Chương trình được dịch bởi Đậu Đậu 5.0"
+echo "[i] Bạn có thể xóa thư mục Raven-Storm ngay bây giờ."
 echo "----------------------------------------"
-echo "[i] Run 'sudo rst' to start Raven-Storm."
+echo "[i] Chạy 'sudo rst' để bắt đầu Raven-Storm."
 echo "----------------------------------------"
 exit 0
